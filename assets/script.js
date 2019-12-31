c9948cf969be5c8e24f8435296976460b557d928
elenagoto <elenagoto@gmail.com>
2019-12-31 13:09:18 +0100

    Add try/catch block to avoid error with 'eval()' function. Clean code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,55 @@
 // 
 const result = document.querySelector('.calculator__result p');
-const resultTemp = document.querySelector('.calculator__result p');
+const resultTemp = document.querySelector('.calculator__result--temp p');
 const keys = document.querySelector('.calculator__keys');
 
 // Functions for operations:
+function tempCalculation(calculArray) {
+  tempCalcul = calculArray.join('');
+  result.textContent = tempCalcul;
+  try {
+    let resultNumber = eval(tempCalcul);
+    if (resultNumber.toString() != tempCalcul) {
+      return resultNumber;
+    } else {
+      return '';
+    }
+  } catch (error) {
+    console.log('operation incompleted');
+  }
+}
 
 function displayCalculation() {
+  // Array to contain the calculation
   let calculation = [];
+  // Event listener
   keys.addEventListener('click',(e) => {
-    if (e.target.textContent === '←'){
-      calculation.pop();
-      result.textContent = calculation.join('');
-    } else if (e.target.textContent === 'C'){
-      calculation = [];
-      result.textContent = calculation.join('');
-    } else {
-      calculation.push(e.target.textContent);
-      result.textContent = calculation.join('');
+    e.preventDefault();
+    // To make sure that the parent elements are ignored
+    if (e.target.classList.contains('calculator__key')) {
+      // if the chosen key is 'erase'
+      if (e.target.textContent === '←'){
+        calculation.pop();
+        resultTemp.textContent = tempCalculation(calculation);
+
+      // if the chosen key is 'reset'
+      } else if (e.target.textContent === 'C'){
+        calculation = [];
+        result.textContent = '';
+        resultTemp.textContent = '';
+      
+      // if the chosen key is 'equal'
+      } else if (e.target.textContent === '='){
+        result.textContent = tempCalculation(calculation);
+        calculation = [];
+        calculation.push(result.textContent);
+        resultTemp.textContent = '';
+      
+      // if the chosen key is any other key
+      } else {
+        calculation.push(e.target.textContent);
+        resultTemp.textContent = tempCalculation(calculation)
+      }
     }
   } );
   
@@ -27,4 +60,3 @@ displayCalculation();
 window.addEventListener('keydown', e => {
   console.log(e.key);
 })
-
