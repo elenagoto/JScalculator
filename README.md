158cb4b9cd1d57587cac7159ce7c1d194dcf6be6
elenagoto <elenagoto@gmail.com>
2020-01-02 22:51:09 +0100

    Modify script to separate in functions different key's actions. -> Modify README to explain goals of the project

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Calculator with JavaScript
 
-This is a simple calculator project to test and practice my JavaScript.
+This is a calculator project to test and practice my JavaScript.
 
-This is one of my Xmas projects for the 2019/2020 holidays.
+## Goals
+
+- The user should be able to make simple arithmetic operations.
+- The user should be able to see the temporary result of his/her calculation while seeing also the whole calculation.
+- The user should be able to erase the last number or operator chosen.
+- The user should be able to reset the whole calculation with a **C** key.
+- The user should be able to get the result of his/her calculation with the **`=`** key, and should be able to use it as the first number of a new calculation.
--- assets/script.js
+++ assets/script.js
@@ -2,61 +2,187 @@
 const result = document.querySelector('.calculator__result p');
 const resultTemp = document.querySelector('.calculator__result--temp p');
 const keys = document.querySelector('.calculator__keys');
+const keysList = document.querySelectorAll('.calculator__key');
 
-// Functions for operations:
-function tempCalculation(calculArray) {
-  tempCalcul = calculArray.join('');
-  result.textContent = tempCalcul;
-  try {
-    let resultNumber = eval(tempCalcul);
-    if (resultNumber.toString() != tempCalcul) {
-      return resultNumber;
+
+// Variables to get the numbers 
+
+let currentNumber = '';
+let currentOperator = '';
+let currentCalculation = [];
+let currentResult = '';
+
+
+
+
+// To manage the Keys containing numbers
+function addNumber(number) {
+  // Needs to add number to 'currentNumber', to 'calculation' and to 'displayCalculation'
+  currentOperator = '';
+  currentNumber = number;
+  currentCalculation.push(number);
+  console.log(currentCalculation);
+}
+
+// To manage the keys containing operators
+function calculation(operator) {
+  // Needs to add operator to 'currentOperator'
+  // If there is no number the only operator to use is minus
+  if (currentNumber !== '') {
+    currentNumber = ''
+    // if other operator and 'currentOperator' is not empty replaces the currentOperator in currentCalculation.
+    if (operator !== '-' && currentOperator !== '') {
+      currentOperator = operator;
+      currentCalculation.pop();
+      currentCalculation.push(operator);
     } else {
-      return '';
+      currentOperator = operator;
+      currentCalculation.push(operator);
     }
-  } catch (error) {
-    console.log('operation incompleted');
+   // if operator is minus and the 'currentOpeartor' is not empty or number is empty, it is use as negative number instead of operator
+  } else if (operator === '-' && currentOperator != '') {
+    currentNumber = operator;
+    currentCalculation.push(operator);
   }
 }
 
-function displayCalculation() {
-  // Array to contain the calculation
-  let calculation = [];
-  // Event listener
-  keys.addEventListener('click',(e) => {
-    e.preventDefault();
-    // To make sure that the parent elements are ignored
-    if (e.target.classList.contains('calculator__key')) {
-      // if the chosen key is 'erase'
-      if (e.target.textContent === '←'){
-        calculation.pop();
-        resultTemp.textContent = tempCalculation(calculation);
-
-      // if the chosen key is 'reset'
-      } else if (e.target.textContent === 'C'){
-        calculation = [];
-        result.textContent = '';
-        resultTemp.textContent = '';
-      
-      // if the chosen key is 'equal'
-      } else if (e.target.textContent === '='){
-        result.textContent = tempCalculation(calculation);
-        calculation = [];
-        calculation.push(result.textContent);
-        resultTemp.textContent = '';
-      
-      // if the chosen key is any other key
-      } else {
-        calculation.push(e.target.textContent);
-        resultTemp.textContent = tempCalculation(calculation)
-      }
+// To manage the keys other keys
+function resultOrErase(changeKey) {
+  // modify the current elements on the display
+  // if erase -> remove last item on display calculation. If 
+  if (changeKey === '←'){
+    currentCalculation.pop();
+    if (currentCalculation.length === 0) {
+      currentResult = '';
     }
-  } );
-  
+
+  // if the chosen key is 'reset'
+  } else if (changeKey === 'C'){
+    currentCalculation = [];
+    currentResult = '';
+
+  // if the chosen key is 'equal'
+  } else if (changeKey === '='){
+    currentCalculation = [];
+    currentCalculation.push(currentResult);
+    currentNumber = currentResult;
+    currentResult = '';
+    
+  }
 }
 
-displayCalculation();
+//  To get the temporary result to display on the 'resultTemp' div
+function resultCalculation() {
+  // eval method will display an error each time strign ends in an operator
+  try {
+    let number = eval(currentCalculation.join(''));
+    if (number != undefined) {
+      currentResult = number;
+    } 
+  } catch (error) {
+    console.log('The result is undefined');
+  }
+}
+
+// Thsi function will use the variables above to display the elements on the page
+function displayCalculation() {
+  result.textContent = currentCalculation.join('');
+  resultTemp.textContent = currentResult;
+  if (result.textContent === resultTemp.textContent) {
+    resultTemp.textContent = '';
+  }
+}
+
+keys.addEventListener('click', (e) => {
+  e.preventDefault();
+  // To make sure that the parent elements are ignored
+  if (e.target.classList.contains('calculator__key')) {
+    if (e.target.classList.contains('key--number')) {
+      addNumber(e.target.textContent);
+    } else if (e.target.classList.contains('key--operator')) {
+      calculation(e.target.textContent);
+    } else if (e.target.classList.contains('key--change')) {
+      resultOrErase(e.target.textContent);
+    }
+  }
+  resultCalculation();
+  displayCalculation();
+
+});
+
+
+
+
+
+// // Functions for operations:
+// function tempCalculation(calculArray) {
+//   tempCalcul = calculArray.join('');
+//   result.textContent = tempCalcul;
+//   try {
+//     let resultNumber = eval(tempCalcul);
+//     if (resultNumber.toString() != tempCalcul) {
+//       return resultNumber;
+//     } else {
+//       return '';
+//     }
+//   } catch (error) {
+//     console.log('operation incompleted');
+//   }
+// }
+
+// function displayCalculation() {
+//   // Array to contain the calculation
+//   let calculation = [];
+//   // Event listener
+//   keys.addEventListener('click',(e) => {
+//     e.preventDefault();
+//     // To make sure that the parent elements are ignored
+//     if (e.target.classList.contains('calculator__key')) {
+//       // if the chosen key is 'erase'
+//       if (e.target.textContent === '←'){
+//         calculation.pop();
+//         resultTemp.textContent = tempCalculation(calculation);
+
+//       // if the chosen key is 'reset'
+//       } else if (e.target.textContent === 'C'){
+//         calculation = [];
+//         result.textContent = '';
+//         resultTemp.textContent = '';
+      
+//       // if the chosen key is 'equal'
+//       } else if (e.target.textContent === '='){
+//         result.textContent = tempCalculation(calculation);
+//         calculation = [];
+//         calculation.push(result.textContent);
+//         resultTemp.textContent = '';
+      
+//       // if the chosen key is any other key
+//       } else {
+//         calculation.push(e.target.textContent);
+//         resultTemp.textContent = tempCalculation(calculation)
+//       }
+//     }
+//   } );
+  
+// }
+
+// displayCalculation();
 
 window.addEventListener('keydown', e => {
-  console.log(e.key);
-})
+  for (key of keysList) {
+    if (e.key === key.textContent) {
+      key.click();
+    } else if (e.key === 'Backspace' && key.textContent === '←') {
+      key.click();
+    } else if (e.key === 'c' && key.textContent === 'C') {
+      key.click();
+    } else if (e.key === 'Enter' && key.textContent === '=') {
+      key.click();
+    }
+
+  }
+});
+
+// for (key of keysList) {
+//   console.log(key);
+// }
