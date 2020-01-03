6bc6daabe7804b17f5ebc8fe90ca5ad6d8782c5a
Laura Gomez <elenagoto@gmail.com>
2020-01-03 16:02:13 +0100

    Modify script to use the operators array to resolve the logic problems-> -> related to the use of the operators. Add some comments as well

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-// 
+// *** VARIABLES ***
+
 const result = document.querySelector('.calculator__result p');
 const resultTemp = document.querySelector('.calculator__result--temp p');
 const keys = document.querySelector('.calculator__keys');
@@ -6,86 +7,86 @@ const keysList = document.querySelectorAll('.calculator__key');
 
 
 // Arrays to compare
-
 const numbers = ['0','1', '2', '3', '4', '5', '6', '8', '9', '.'];
 const operators = ['+', '-', '*', '/'];
-const modifiers = ['=', 'C', '←']
 
 // Variables to store the calculation and the result
 let currentCalculation = [];
-let currentElement = currentCalculation[currentCalculation.length - 1];
+let currentElement = '';
 let currentResult = '';
 
 // Var to know if the user has started a calculation or nor
 let newCalculation = false;
 
 
-
-
+// **** FUNCTIONS **** 
 // To manage the Keys containing numbers
 function addNumber(number) {
   // We need to know if we have to add a zero before a dot
   let zero = '';
-  for (operator of operators) {
-    if (number === '.' && (newCalculation === false || currentElement === operator)) {
+  for (element of operators) {
+    if (number === '.' && (newCalculation === false || currentElement === element)) {
       zero = '0';
     }
   }
+  // Start calculation
   newCalculation = true;
+  // Add number to the currentCalculation Array
   currentCalculation.push(zero + number);
+
   console.log(currentCalculation);
 }
 
 // To manage the keys containing operators
 function calculation(operator) {
-  // Needs to add operator to 'currentOperator'
-  // If there is no number the only operator to use is minus
-  if (newCalculation === true) {
-    // if other operator and 'currentOperator' is not empty replaces the currentOperator in currentCalculation.
-    if (currentOperator !== '' && operator != '-') {
-      currentOperator = operator;
-      currentCalculation.pop();
+  // if the operator is minus, always add it
+  if (operator === '-' && currentElement !== '-') {
       currentCalculation.push(operator);
-    } else if (currentOperator === '') {
-      currentOperator = operator;
-      currentCalculation.push(operator);
+  // If the operator is other than minus, it can only be added after a number
+  } else if (newCalculation === true) {
+    // The function has to determine if the current element is a number or operator
+    let replace = false;
+    for (element of operators) {
+      // replace will be true if current element is an operator
+      if (currentElement === element) {
+        replace = true;
+      } 
     }
-   // if operator is minus and the 'currentOpeartor' is not empty or number is empty, it is use as negative number instead of operator
-  } else if (operator === '-' && (currentOperator != '' || newCalculation === false)) {
-    currentNumber = operator;
+    //  if replace is true, new operator replaces the previous one. 
+    if (replace === true) {
+      // remove previous operator
+      currentCalculation.pop();
+    }
+    // Add operator to array
     currentCalculation.push(operator);
   }
 }
 
 // To manage the keys other keys
-function resultOrErase(changeKey) {
-  // modify the current elements on the display
+function modifyCalculation(changeKey) {
+
   // if erase -> remove last item on display calculation. If 
   if (changeKey === '←'){
+    // Remove last item in the currentCalculation array
     currentCalculation.pop();
-    currentNumber = '';
-    currentOperator = '';
+    // if the item removed was the last, reset the other variables
     if (currentCalculation.length === 0) {
       currentResult = '';
       newCalculation = false;
     }
     
-  // if the chosen key is 'reset'
+  // if the chosen key is 'reset', reset all the variables
   } else if (changeKey === 'C'){
     newCalculation = false;
     currentCalculation = [];
+    currentElement = '';
     currentResult = '';
-    currentNumber = '';
-    currentOperator = '';
 
-  // if the chosen key is 'equal'
+  // if the chosen key is 'equal' use the current result as the first element in the array
   } else if (changeKey === '='){
     currentCalculation = [];
     currentCalculation.push(currentResult);
-    currentNumber = currentResult;
     currentResult = '';
-    currentOperator = '';
-    
   }
 }
 
@@ -105,17 +106,21 @@ function resultCalculation() {
 // Thsi function will use the variables above to display the elements on the page
 function displayCalculation() {
   result.textContent = currentCalculation.join('');
-  resultTemp.textContent = currentResult;
+  if (currentCalculation.length > 1) {
+    resultTemp.textContent = currentResult;
+  } else {
+    resultTemp.textContent = '';
+  } 
 }
 
 function modifyFont() {
-  if (result.textContent.length >= 13) {
+  if (result.textContent.length >= 14) {
     result.setAttribute('class', 'result--small');
   } else {
     result.removeAttribute('class');
   }
 
-  if (resultTemp.textContent.length >= 15) {
+  if (resultTemp.textContent.length >= 16) {
     resultTemp.setAttribute('class', 'temp--small');
   } else {
     resultTemp.removeAttribute('class');
@@ -130,16 +135,16 @@ keys.addEventListener('click', (e) => {
       addNumber(e.target.textContent);
     } else if (e.target.classList.contains('key--operator')) {
       calculation(e.target.textContent);
-    } else if (e.target.classList.contains('key--change')) {
-      resultOrErase(e.target.textContent);
+    } else if (e.target.classList.contains('key--modifier')) {
+      modifyCalculation(e.target.textContent);
     }
   }
   resultCalculation();
   displayCalculation();
   modifyFont();
+  currentElement = currentCalculation[currentCalculation.length - 1];
 
-  console.log(`The currentNumber is: ${currentNumber} ---
-  The current operator is ${currentOperator} ---
+  console.log(`The current element is ${currentElement} ---
   The current calculation is ${currentCalculation} ---
   The current Result is ${currentResult} ---`)
 });
